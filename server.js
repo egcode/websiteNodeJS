5f77028a785a9214c16ee77a167a1f38b6417b8f
eg <eugene@arcules.com>
2019-04-16 10:42:00 -0700

    aa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');    //Express Web Server
 var path = require('path');     //used for file path
 var bodyParser = require('body-parser')
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 443;
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
