609ebd1e8a35243eaa8267bd3c2e657534072f71
eg <eugene@arcules.com>
2019-04-16 09:29:44 -0700

    port

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');    //Express Web Server
 var path = require('path');     //used for file path
 var bodyParser = require('body-parser')
 
+var port = process.env.PORT || 3030;
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
  
 // parse application/json
 app.use(bodyParser.json())
 
-var server = app.listen(3030, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
